e32bd69b7918c4f1d8e4ddde364deae876e8c335
Wachirawut Thamviset <twachikku@gmail.com>
2024-10-31 23:15:13 +0700

    file.vue.php
    แก้ไขไฟล์ จาก .php  เป็น  .vue.php

--- src/Controllers/VueController.php
+++ src/Controllers/VueController.php
@@ -2,42 +2,38 @@
 
 namespace twachikku\Vue3cdn\Controllers;
 
-use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
-use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 
-class VueController 
+class VueController
 {
-    use AuthorizesRequests, ValidatesRequests;
-
-    var $request;
-    var $user = null;
     var $phpfilepath = "vue3";
-    public function __construct(Request $request)
+
+    var $resource_paths = [];
+
+
+    public function index($appid, $pageid = "", $args = [])
     {
-        $this->request = $request;
-        $this->user = $request->user();
-    }
-    public function index($appid = "public", $pageid = "home", $args = [])
-    {
+        if ($pageid == "") {
+            $pageid = $appid;
+            $appid = "";
+        }
+        if ($pageid == "" || $pageid[0] == "_") {
+            return response("Access denied <a href='/'>OK</a>", 505);
+        }
         $ext = "";
         $p = strpos($pageid, ".");
         if ($p > 0) {
-            $ext = file_ext($pageid);
+            $ext = self::file_ext($pageid);
             $pageid = substr($pageid, 0, $p);
         }
-        if (in_array($pageid, ["_config", "_menu", "_right"])) {
-            return response("Error: access denied <a href='/'>OK</a>", 505);
+        if ($appid == "api" || $appid == "json" || $ext == ".json") {
+            return $this->json($appid, $pageid);
         }
-        if ($appid == "json" || $ext==".json") {
-            return $this->json($appid,$pageid);
-        }
-        
         $error = null;
         $files = $this->getFiles($appid, $pageid);
-        $method = strtolower($this->request->getMethod());
+        $method = strtolower(request()->getMethod());
         $pagename = str_replace("/", "_", $files['name']);
         $props = ['app' => $appid, 'id' => $files['name'], "args" => $args];
         $props['head'] = "";
@@ -57,27 +53,21 @@ class VueController
         }
 
         $page = ['name' => $pagename];
-
-        $page['files'] = $files;
+        //$page['files'] = $files;
         $page['title'] = "$appid/$pageid";
         $data = [];
-        $user = $this->user;
-        if ($user) {
-            $page['user'] = $user;
-            $page['user_data'] = $user->data()->data;
-        }
+
         $props['contentType'] = request("contentType", ($method == "post") ? 'json' : '');
 
         $response = null;
         ob_start();
-        if (file_exists(base_path($files['header']))) {
-
-            $response = include(base_path($files['header']));
+        if ($files['header']!==false) {
+            $response = include($files['header']);
         }
-        if (file_exists(base_path($files['php']))) {
-            $response = include(base_path($files['php']));
+        if ($files['php']!==false) {
+            $response = include($files['php']);
         } else {
-            return response("Error: $appid/$pageid is not found. <a href='/'>OK</a>", 404);
+            return $this->file_not_found($appid, $pageid);
         }
         $content = ob_get_contents();
         ob_end_clean();
@@ -91,7 +81,6 @@ class VueController
         if (data_get($props, 'contentType') == "json") {
             if ($content != '')
                 $props['console'] = $content;
-            //$props['$page'] = $page;
             return $this->response_json($props);
         }
         if (isset($props['redirect'])) {
@@ -101,25 +90,25 @@ class VueController
             return $response;
         }
 
-        if (isset($files['php_left'])) {
+        if ($files['php_left']!==false) {
             ob_start();
-            include(base_path($files['php_left']));
+            include($files['php_left']);
             $props['content_left'] = trim(ob_get_contents());
             ob_end_clean();
             if ($props['content_left'] == "")
                 unset($props['content_left']);
         }
-        if (isset($files['php_right'])) {
+        if ($files['php_right']!==false) {
             ob_start();
-            include(base_path($files['php_right']));
+            include($files['php_right']);
             $props['content_right'] = trim(ob_get_contents());
             ob_end_clean();
             if ($props['content_right'] == "")
                 unset($props['content_right']);
         }
         if (isset($props['layout'])) {
-            $file = resource_path("{$this->phpfilepath}/layouts/{$props['layout']}.php");
-            if (file_exists($file)) {
+            $file = $this->get_layoutfile("{$props['layout']}.vue.php","{$props['layout']}.php");
+            if ($file!==false) {
                 ob_start();
                 include($file);
                 $content = ob_get_contents();
@@ -129,41 +118,26 @@ class VueController
         $this->extract_template($content, $props);
         $props['page'] = $page;
         $props['data'] = $data;
-        return view($this->phpfilepath."::index", $props);
+        return view($this->phpfilepath . "::index", $props);
     }
 
 
-    function getFiles($appid, $pageid="")
+    function getFiles($appid, $pageid = "")
     {
-        global $app;
-        $method = strtolower($this->request->getMethod());
+        $request = request();
+        $file = "$appid/$pageid";
+        if ($appid == "")
+            $file = $pageid;
+        $method = strtolower($request->getMethod());
         $f = [];
-        $path = "resources/{$this->phpfilepath}/$appid";
-        $f['name'] = strtolower("$appid/$pageid");
-        if($pageid==""){
-            $path = "resources/{$this->phpfilepath}";
-            $f['name'] =  strtolower("$appid");
-        }
-
-        $f['header'] = "resources/{$this->phpfilepath}/$appid/_config.php";
+        $f['name'] = strtolower($file);
+        $f['header'] = $this->get_phpvuefile("$appid/_config.php");
         if ($method == 'get') {
-            $f['php_left'] = "resources/{$this->phpfilepath}/$appid/_menu.php";
-            $f['php_right'] = "resources/{$this->phpfilepath}/$appid/_right.php";
-            $f['php'] = "resources/{$this->phpfilepath}/{$f['name']}.php";
-            if (!file_exists(base_path($f['php_left']))) {
-                unset($f['php_left']);
-            }
-            if (!file_exists(base_path($f['php_right']))) {
-                unset($f['php_right']);
-            }
-            $r = "resources/{$this->phpfilepath}/{$f['name']}_right.php";
-            if (file_exists(base_path($r))) {
-                $f['php_right'] = $r;
-            }
-
+            $f['php_left'] = $this->get_phpvuefile("$appid/_menu.vue.php","$appid/_menu.php");
+            $f['php_right'] = $this->get_phpvuefile("$appid/_menu.vue.php","$appid/_right.php");
+            $f['php'] = $this->get_phpvuefile("{$f['name']}.vue.php","{$f['name']}.php");
         } else {
-            
-            $f['php'] = "resources/{$this->phpfilepath}/{$f['name']}.$method.php";
+            $f['php'] = $this->get_phpvuefile("{$f['name']}.vue.php","{$f['name']}.$method.php");
         }
         return $f;
     }
@@ -211,31 +185,96 @@ class VueController
 
     function response_json($props)
     {
-        return response(json_zipencode($props),200,['Content-Type: application/json; charset=utf-8']);
+        return response($this->json_zipencode($props), 200, ['Content-Type: application/json; charset=utf-8']);
     }
-    function json($appid,$pageid)
+    function json($appid, $pageid)
     {
-        $request = $this->request;
-        $path = resource_path($appid);
-        $res = [];
-        $ext = file_ext($pageid);
-        $found = false;
-        if (file_exists(("$path/$pageid.json"))) {
-            $res = file_get_json("$path/$pageid.json");
-            $found=true;
+        $request = request();
+        $file = "$appid/$pageid";
+        if ($appid == "")
+            $file = $pageid;
+        $method = strtolower($request->getMethod());
+
+        if ($method == "get") {
+            $filepath = $this->get_phpvuefile("$file.json");
+            if ($filepath != false) {
+                return $this->response_json(file_get_contents($filepath));
+            }
         }
-        if (file_exists(("$path/$pageid.php"))) {
-            include("$path/$pageid.php");
-            $found=true;
+        $filepath = ($method == "get") ? $this->get_phpvuefile("$file.php") : $this->get_phpvuefile("$file.$method.php");
+        if ($filepath != false) {
+            $res = [];
+            include($filepath);
+            return $this->response_json($res);
         }
-        if($found) return $this->response_json($res);
-        else return $this->file_not_found($appid,$pageid);
+        return $this->file_not_found($appid, $pageid);
     }
-    function file_not_found($appid,$pageid){
+    function file_not_found($appid, $pageid)
+    {
         // todo เพิ่มการป้องการ โจรกรรม นับจำนวนครั้งที่ โหลดผิด
         // $n = session('file_not_found',0)+1;
         // session(['file_not_found'=>$n]);
         // return response("Error: $appid/$pageid is not found.($n times) <a href='/'>OK</a>", 404);
         abort(404);
+    }
+
+    static function file_ext($fname)
+    {
+        $p = strrpos($fname, ".");
+        if ($p > 0) {
+            return substr($fname, $p);
+        }
+        return "";
+    }
+
+    function get_filepath($filename)
+    {
+        $paths = [
+            resource_path(),
+            realpath(__DIR__ . "../resources")
+        ];
+        foreach ($paths as $p) {
+            $f = "$p/$filename";
+            if (file_exists($f))
+                return $f;
+        }
+        return false;
+    }
+    function get_phpvuefile(...$filenames)
+    {
+        foreach ($filenames as $filename) {
+            if ($filename[0] == "/")
+                $filename = substr($filename, 1);
+            $p = $this->get_filepath($this->phpfilepath . "/$filename");
+            if ($p != false)
+                return $p;
+        }
+        return false;
+    }
+    function get_layoutfile(...$filenames)
+    {
+        foreach ($filenames as $filename) {
+            if ($filename[0] == "/")
+                $filename = substr($filename, 1);
+            $p = $this->get_filepath("layouts/$filename");
+            if ($p != false)
+                return $p;
+        }
+        return false;
+    }
+
+    static function json_zipencode($data)
+    {   
+        $json = (is_string($data))?$data:json_encode($data, JSON_UNESCAPED_UNICODE);
+        $size1 = strlen($json);
+        if ($size1 > 2024) {
+            $data = base64_encode(zlib_encode($json, ZLIB_ENCODING_DEFLATE));
+            $size2 = strlen($data);
+            if ($size1 > $size2) {
+                $json = ['zip' => true, "len" => $size1, 'data' => $data];
+                return json_encode($json);
+            }
+        }
+        return $json;
     }
 }
